ef43529ea903915e3b598a7674a853c16f60af0b
SangWonKo <swko1017@naver.com>
2023-02-01 02:52:43 +0900

    feat: 브라우저 탭 추가 & 삭제 기능 구현

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,8 @@
+import { browserTabIdState } from '@store/atom'
 import { Layout, Tabs } from 'antd'
+import { KeyboardEvent, MouseEvent, useEffect, useRef } from 'react'
 import { Outlet, useNavigate, useParams } from 'react-router-dom'
+import { useRecoilState } from 'recoil'
 import styled from 'styled-components'
 
 const StyledLayout = styled(Layout)`
@@ -64,6 +67,31 @@ const { Content } = StyledLayout
 const MainLayout = () => {
   const navigate = useNavigate()
   const { tab } = useParams()
+  const [tabIds, setTabIds] = useRecoilState(browserTabIdState)
+  const newTabIndex = useRef(2)
+
+  const handleEdit = (
+    targetKey: MouseEvent | KeyboardEvent | string,
+    action: 'add' | 'remove'
+  ) => {
+    if (action === 'add') {
+      const newTab = `tab${newTabIndex.current++}`
+      setTabIds((prev) => [...prev, newTab])
+      navigate(newTab)
+    } else {
+      const newTabs = tabIds.filter((tabId) => tabId !== targetKey)
+      setTabIds(newTabs)
+      navigate(newTabs[newTabs.length - 1])
+    }
+  }
+
+  useEffect(() => {
+    if (!tab) return
+
+    if (!tabIds.includes(tab)) {
+      navigate(tabIds[tabIds.length - 1])
+    }
+  }, [tab, tabIds, navigate])
 
   return (
     <StyledLayout>
@@ -71,11 +99,13 @@ const MainLayout = () => {
         <StyledTabs
           type="editable-card"
           onChange={(path) => navigate(`${path}`)}
+          onEdit={handleEdit}
           activeKey={tab}
-          items={[
-            { label: 'tab1', key: 'tab1', children: <Outlet /> },
-            { label: 'tab2', key: 'tab2', children: <Outlet /> },
-          ]}
+          items={tabIds.map((tabId) => ({
+            label: tabId,
+            key: tabId,
+            children: <Outlet />,
+          }))}
           destroyInactiveTabPane
         />
       </Content>
